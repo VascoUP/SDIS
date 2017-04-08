cb2467f9ed4cd9b5fe17857f9588940a10ca6876
saracouto1318 <saracouto1318@hotmail.com>
2017-04-08 19:20:27 +0100

    RMI commented

--- Backup/src/rmi/Instructable.java
+++ Backup/src/rmi/Instructable.java
@@ -3,6 +3,11 @@ package rmi;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
+/**
+ * 
+ * This class builds rmi's instructable that extends the Remote class
+ *
+ */
 public interface Instructable extends Remote {
 	public void run(String[] args) throws RemoteException;
 }
--- Backup/src/rmi/RMIRunner.java
+++ Backup/src/rmi/RMIRunner.java
@@ -6,8 +6,17 @@ import protocol.Protocol;
 import protocol.Restore;
 import threads.ThreadManager;
 
+/**
+ * 
+ * This class builds a RMI's runner
+ *
+ */
 public class RMIRunner {
 	
+	/**
+	 * Parses the different rmi's arguments
+	 * @param rmiArgs Arguments that will be parsed
+	 */
 	public static void parseArgs(String[] rmiArgs) {
 		if( rmiArgs.length < 1 )
 			return ;
@@ -16,6 +25,7 @@ public class RMIRunner {
 		String path;
 		int rep_degree;
 		
+		//Different protocols
 		switch(protocol) {
 		case "BACKUP":
 			if( rmiArgs.length != 3 )
@@ -44,12 +54,20 @@ public class RMIRunner {
 		return ;
 	}
 
+	/**
+	 * Initiates the backup's service
+	 * @param path File's pathname
+	 * @param rep_degree Replication's degree
+	 */
 	public static void backUp(String path, int rep_degree) {
 		System.out.println("Backup");
 		Protocol backup = new BackUp(path, rep_degree);
 		backup.run_service();
 	}
 	
+	/**
+	 * Closes the threads and finishes the services
+	 */
 	public static void close() {
 		System.out.println("Close");	
 		ThreadManager.closeThreads();
@@ -57,6 +75,10 @@ public class RMIRunner {
 		//RMIStorage.getRMI().unbind();
 	}
 	
+	/**
+	 * Initiates the delete's protocol
+	 * @param path File's pathname
+	 */
 	public static void delete(String path) {
 		System.out.println("Delete");
 		Protocol delete;
@@ -67,6 +89,10 @@ public class RMIRunner {
 		}
 	}
 	
+	/**
+	 * Initiates the restore's protocol
+	 * @param path File's pathname
+	 */
 	public static void restore(String path) {
 		System.out.println("Restore");		
 		try {
