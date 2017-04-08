b49f67ef1c91317044fc40a9d5f8a3f55ff2a3b2
saracouto1318 <saracouto1318@hotmail.com>
2017-04-08 09:32:51 +0100

    default package commented

--- Backup/src/Peer.java
+++ Backup/src/Peer.java
@@ -3,7 +3,15 @@ import information.PeerInfo;
 import rmi.RMIStorage;
 import threads.ThreadManager;
 
+/**
+ * 
+ * This class builds a peer
+ *
+ */
 public class Peer {	
+	/**
+	 * This function initiates the file information, the thread manager and the RMI storage
+	 */
 	public static void initAll() {
 		FileInfo.init();
 		ThreadManager.initThreadManager();
@@ -11,9 +19,14 @@ public class Peer {
 		RMIStorage.getRMI().bind();
 	}
 	
+	/**
+	 * Main function to execute the peer's
+	 * @param args Arguments passed by the terminal/command line
+	 */
 	public static void main(String[] args) {
 		if( args.length != 3 )
 			return ;
+		//Creates the peer's information
 		PeerInfo.createPeerInfo(args[0], args[2], Integer.parseInt(args[1]));
 		initAll();		
 	}
--- Backup/src/TestApp.java
+++ Backup/src/TestApp.java
@@ -5,38 +5,70 @@ import java.rmi.registry.Registry;
 
 import rmi.Instructable;
 
+/**
+ * 
+ * Class that creates the test application to test all protocols
+ *
+ */
 public class TestApp {	
 	private static TestApp testApp;
 	
-	/*==============
+	/*
+	 *==============
 	 * MAIN METHODS
 	 *==============
+	 */
+	
+	/**
+	 * Main function to execute the test application
+	 * @param args Arguments passed by the terminal/command line
 	 */
     public static void main(String[] args) {
     	parseArgs(args);
     }
+    
+    /**
+     * This function parses the arguments passed by the terminal/command line
+     * @param args Arguments passed by the terminal/command line
+     */
 	public static void parseArgs(String[] args) {    	
     	String[] rmiArgs = new String[args.length - 1];
     	System.arraycopy(args, 1, rmiArgs, 0, args.length - 1);
     	
+    	//Initiates the TestApp
     	testApp = new TestApp();
     	if (testApp.getRMI(args[0]) )
     		testApp.runRMI(rmiArgs);    	
     }
 	
-	
+	/**
+	 * RMI registry
+	 */
 	private Registry registry;
     
+	/**
+	 * Interface to RMI instructable
+	 */
     private Instructable stub;
     
     
-    /*====================
+    /*
+     *====================
      * NON STATIC METHODS
      *====================
+     */
+    
+    /**
+     * TestaApp constructor
      */
     public TestApp() {
     }
 
+    /**
+     * Creates the RMI with the peer's name
+     * @param peer_name Name of the peer which will be registered
+     * @return false if the registry catches some exception or true otherwise 
+     */
     public boolean getRMI(String peer_name) {
         try {
 			testApp.registerRMI(peer_name);
@@ -51,11 +83,21 @@ public class TestApp {
         return true;
     }
     
+    /**
+     * Registers the peer in RMI
+     * @param peer_name Name of the peer which will be registered
+     * @throws RemoteException To a number of communication-related exceptions that may occur during the execution of a remote method call
+     * @throws NotBoundException This is thrown if an attempt is made to lookup or unbind in the registry a name that has no associated binding
+     */
     public void registerRMI(String peer_name) throws RemoteException, NotBoundException {
         registry = LocateRegistry.getRegistry();
         stub = (Instructable) registry.lookup(peer_name);
     }
     
+    /**
+     * Runs the RMI 
+     * @param rmiArgs Arguments used to run the RMI
+     */
     public void runRMI(String[] rmiArgs) {
     	try {
 			stub.run(rmiArgs);
