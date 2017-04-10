caa5c14766213146ca27c607134202491b4da752
saracouto1318 <saracouto1318@hotmail.com>
2017-04-10 16:45:24 +0100

    scriptLinux

---
+++ Backup/src/scriptLinux.sh
@@ -0,0 +1,1 @@
+javac connection/*.java file/*.java information/*.java listener/*.java message/*.java protocol/*.java rmi/*.java sender/*.java spacemanaging/*.java threads/*.java workerHandlers/*.java *.java
